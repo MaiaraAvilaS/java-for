32e234cff1a06d275fafee750b4e386478d088cb
Maiara Ávila <maiaroviski@gmail.com>
2026-02-02 10:38:33 -0300

    Incluindo arquivos

---
+++ app/src/main/java/com/example/EstruturaFor/App.java
@@ -0,0 +1,14 @@
+
+package com.example.EstruturaFor;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 10; i++ ){ //1o variável de controle | 2o condição de parada(enquanto for 10) | 3o incremento (i+1)
+            
+        }
+
+    }
+}
